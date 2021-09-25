660999dbe9d4a8994472d86cb546f42cdd622cd8
Madara <madara.bieza@gmail.com>
2021-09-25 23:07:22 +0300

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay.js";
 
